f50848ec03d207a9765613840b4f8fec1b6d2e69
lazyframework <lazyframeworkid@gmail.com>
2026-05-21 12:19:15 +0000

    first commit

--- bin/console.py
+++ bin/console.py
@@ -7,11 +7,6 @@ import select
 import json
 import requests
 from pathlib import Path
-# ====================== FORCE PATH ======================
-PROJECT_ROOT = Path(__file__).resolve().parent.parent
-if str(PROJECT_ROOT) not in sys.path:
-    sys.path.insert(0, str(PROJECT_ROOT))
-# =======================================================
 
 from dataclasses import dataclass, field
 from typing import Dict, Any, Optional, List
@@ -36,6 +31,8 @@ os.environ['COLORTERM'] = 'truecolor'
 console = Console()
 import builtins
 builtins.console = console
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(PROJECT_ROOT))
 
 BASE_DIR = PROJECT_ROOT
 MODULE_DIR = BASE_DIR / "modules"
